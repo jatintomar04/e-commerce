1b3957cbb4b6c0705fafa4a7c6e2b8d1c4e71262
Jatin Singh Tomar <Jatin.tomar.04@gmail.com>
2025-03-22 14:20:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Slider from './components/Slider'
 import Home from './pages/Home'
 import Products from './components/Products'
